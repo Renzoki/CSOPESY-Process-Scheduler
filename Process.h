c566bfffc7489e119445fdd81a361bc2f5846803
Lara-M-F <lara_fourrier@dlsu.edu.ph>
2025-11-04 23:34:25 +0800

    Update Process.h
    improved structure and added barebones process instructions
    currently has a problem with PRINT

--- Process.h
+++ Process.h
@@ -12,17 +12,22 @@ struct Instruction {
 class Process {
 public:
     Process(const std::string& name, const std::vector<Instruction>& instructions);
+
     std::string getName() const;
     bool isFinished() const;
     void setFinished(bool f);
+
     size_t getCurrentLine() const;
     size_t getTotalLines() const;
-    void advanceLine();
+
     uint16_t getVariable(const std::string& name) const;
-    void setVariable(const std::string& name, uint16_t value); 
-    const std::vector<std::string>& getLogs() const; 
-    void addLog(const std::string& msg); 
-    void executeNextInstruction();
+    void setVariable(const std::string& name, uint16_t value);
+
+    const std::vector<std::string>& getLogs() const;
+    void addLog(const std::string& msg);
+
+    void executeNextInstruction(int nestedLevel = 0); // execute current instruction
+    void executeInstruction(const Instruction& instr, int nestedLevel = 0);
 
 private:
     std::string name;
@@ -31,4 +36,11 @@ private:
     size_t current_line;
     std::map<std::string, uint16_t> variables;
     std::vector<std::string> logs;
+
+    // helpers
+    bool isNumber(const std::string& s) const;
+    uint16_t getValue(const std::string& token) const;
+    uint16_t clampUint16(int64_t v) const;
+
+    std::string instrTypeAsString(Instruction::Type type) const;
 };
