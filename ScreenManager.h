19edee144ba68c2c23548e001cdce8226ee0ff31
Lara-M-F <lara_fourrier@dlsu.edu.ph>
2025-11-05 14:35:41 +0800

    added necessary for screen -util

--- ScreenManager.h
+++ ScreenManager.h
@@ -11,4 +11,5 @@ public:
     static void createAndAttach(const std::string& name); 
     static void addProcess(const Process& p);
     static std::vector<Process>& getProcesses();
+    void printUtilizationReport(bool toFile);
 };
